aeffc8158b54970c1c7993cd4840cbd0e4f90bc3
Grady Robbins <gradyrobbins13@gmail.com>
2019-05-01 10:11:38 -0700

    initial Models

---
+++ FourthStar1/Models/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FourthStar1.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace StudentExercises.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() { }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+
+        // Set up PK -> FK relationships to other objects
+        public virtual ICollection<Drill> Drills { get; set; }
+        public virtual ICollection<Team> Teams { get; set; }
+
+    }
+}
---
+++ FourthStar1/Models/Drill.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FourthStar1.Models
+{
+    public class Drill
+    {
+
+       
+
+
+    // You must define a type for representing an drill. 
+
+    
+   
+        
+
+
+
+            public int Id { get; set; }
+
+            [Display(Name = "Drill Name")]
+            public string DrillName { get; set; }
+
+            [Display(Name = "Description")]
+            public string DrillDescription { get; set; }
+
+            [Display(Name = "Players Required")]
+            public int PlayersRequired { get; set; }
+
+            [Display(Name = "Date Created")]
+            public DateTime DateCreated { get; set; }
+
+    }
+
+}
+
+
+
+
+
+
+
+
